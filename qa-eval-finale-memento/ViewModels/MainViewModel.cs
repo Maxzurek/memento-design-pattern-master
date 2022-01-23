f29fa7611f4872e7b7270b098fcbe5eaa2079ab9
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-23 14:50:23 -0500

    Update MainViewModel.cs

--- qa-eval-finale-memento/ViewModels/MainViewModel.cs
+++ qa-eval-finale-memento/ViewModels/MainViewModel.cs
@@ -144,8 +144,6 @@ namespace qa_eval_finale_memento.ViewModels
             }
             else
             {
-                List<string> baseInputs = new("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0".Split(","));
-
                 if (Text.Length == 0)
                 {
                     return;
